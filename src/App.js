97618aed6d15837d56458fc1340c8b2e55e66362
Fernando Bonfim <devnoob@vivaldi.net>
2022-08-14 11:11:13 -0300

    Added a new field to form.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React from "react"
+
 export default class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      firstName: "",
+      lastName: ""
+    };
+  }
+
+  handleChange = (e) => {
+    let {name, value} = e.target
+    this.setState({
+      [name]: value
+    });
+
+    console.log(this.state[name])
+  }
 
   render(){
     return (
       <main>
         <form>
-          <label>First name: </label>
-          <input name="firstName" onChange={handleChange}> </input>
+
+          <label htmlFor="firstName">First name: </label>
+
+          <input name="firstName" id="firstName" value={this.state.firstName}
+          onChange={this.handleChange}/>
+
+          <label htmlFor="lastName"> Last name: </label>
+            <input name="lastName" id="lastName" value={this.state.lastName}
+          onChange={this.handleChange}/>
+
+
         </form>
       </main>
     )
